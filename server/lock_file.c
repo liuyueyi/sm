da5fbec86ab1203a2d521bc49fdbb77e1a8df575
wzb <bangzewu@126.com>
2014-09-03 19:33:24 +0800

    add description

--- server/lock_file.c
+++ server/lock_file.c
@@ -1,13 +1,20 @@
+/*
+ * lock_file.c
+ *  对文件上锁，用于测试server中接受client发送的文件时，重改名的问题
+ *		若文件上锁，则server接受到文件并保存为临时文件(rsa_key.conf.tm)，并等待文件解锁；当文件解锁后，将临时文件重命名为所需的文件名(rsa_key.conf)
+ *  Created on: 2014-8-29
+ *      Author: wzb
+ */
 #include <sys/file.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-	FILE *f = fopen("test2", "r");
+	FILE *f = fopen("rsa_key.conf", "r");
 	if(NULL == f)
 	{
-		fprintf(stderr, "test2 filename error\n");
+		fprintf(stderr, "filename error\n");
 		return 0;
 	}
 	
